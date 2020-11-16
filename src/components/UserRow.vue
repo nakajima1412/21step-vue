f7406009f2e0a962146edb7a76c67c9971008be6
nakajima1412 <nakajima1412@icloud.com>
2020-11-17 00:32:52 +0900

    Add: user row

---
+++ src/components/UserRow.vue
@@ -0,0 +1,51 @@
+<template>
+  <tr>
+    <td>
+      <span v-if="!editable" @click="edit()">{{ user.nickname }}</span>
+      <input
+        v-show="editable"
+        ref="editNickname"
+        :user="user"
+        @input="this.$emit('updateUserNickName', $event.target.value)"
+        @blur="editable = false"
+      />
+    </td>
+    <td>{{ user.email }}</td>
+  </tr>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export function User(nickname, email) {
+  this.nickname = nickname
+  this.email = email
+}
+
+export default defineComponent({
+  props: {
+    user: {
+      type: User,
+      required: true,
+    },
+  },
+  data: function() {
+    return { editable: false }
+  },
+  methods: {
+    edit: function() {
+      this.editable = true
+      this.$nextTick(() => {
+        // DOM更新後に実行
+        this.$refs.editNickname.focus()
+      })
+    },
+  },
+})
+</script>
+
+<style module>
+td input {
+  width: 95%;
+}
+</style>
